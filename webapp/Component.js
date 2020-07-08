a98ae8f6df9ee3e99059ef081c53241942e1d145
Matheus Oliveira Goulart <matheus.goulart@fh.com.br>
2020-07-08 23:55:43 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ui/generic/app/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("com.mf.sap.fi.cobrancas.PDDAnalysis.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
